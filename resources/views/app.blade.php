d4e4396042dbbd7768652bbbd1f9064f25322353
Luciano <luciano.english@gmail.com>
2026-01-29 10:57:30 -0500

    Add favicon.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,9 +30,8 @@
             }
         </style>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-        <link rel="icon" href="/favicon.ico" sizes="any">
+        <title inertia>{{ config('app.name', 'Palm Coast Vape') }}</title>
+        
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
