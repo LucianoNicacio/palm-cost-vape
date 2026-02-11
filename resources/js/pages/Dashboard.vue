6552314f06a714e2bf06c240a90573de1cfa7b58
Luciano <luciano.english@gmail.com>
2026-02-11 14:08:37 -0500

    Replace wayfinder with simple route helpers

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
-import { home } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Home',
-        href: home().url,
+        href: '/',
     },
 ];
 </script>
