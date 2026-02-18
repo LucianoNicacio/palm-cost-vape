fb68fbff1b5648c4bfecc32241e6e43ff7837e31
Luciano <luciano.english@gmail.com>
2026-02-18 14:03:21 -0500

    Changing timezone.

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/New_York',
 
     'age_requirement' => env('AGE_REQUIREMENT', 21),
     'tax_rate' => env('TAX_RATE', 0.06),
